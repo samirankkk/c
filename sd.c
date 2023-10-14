d47aaeef5668e05797cdd947ed2935af699a7dfe
samirankkk <145955750+samirankkk@users.noreply.github.com>
2023-10-14 11:37:35 +0530

    Add files via upload

---
+++ sd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int mat[2][2],i,j;
+    printf("Enter matrix elements");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+    {
+        scanf("%d",&mat[i][j]);
+    }
+    }
+    printf("Matrix elements:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+    {
+        printf("%d ",mat[i][j]);
+    }    
+    printf("\n");
+    }
+    return 0;
+}
